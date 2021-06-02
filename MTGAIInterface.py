2cfe4d6a7f95423e2e1701c961860cc06ac5182a
gurgi414 <gurgithemastermind@gmail.com>
2021-06-02 12:54:02 -0500

    Began making GameState type under MTGGame

---
+++ MTGAIInterface.py
@@ -0,0 +1,19 @@
+class MTGAI:
+    """
+    Informal Interface
+    Defines kernel methods for the AI type.
+    """
+
+    def generate_move(self, game_state: (str, ...)) -> str:
+        """
+        Takes in the game state of a game and returns
+
+        Parameters:
+            game_state (str, ..., str): a valid game state of a game
+        Requires:
+            game_state is of a valid form
+        Returns:
+            An allowed move in String format
+        """
+        pass
+
--- MTGGame.py
+++ MTGGame.py
@@ -2,7 +2,11 @@ from ControllerInterface import Controller
 from MTGAController import MTGAController
 
 
-class Game:
+class MTGGame:
+    class GameState:
+        def __init__(self, permanent_id_array, ):
+            self.permanent_id_array = permanent_id_array
+
     def __init__(self, controller: Controller = MTGAController()):
         controller.start_game()
         while not controller.game_over():

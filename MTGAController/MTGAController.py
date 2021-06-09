1990f5169f58dddbc246c66c8bd03e783d91e0e2
gurgi414 <gurgithemastermind@gmail.om>
2021-06-09 11:44:42 -0400

    Finished and tested MTGALogReader.py. Did not write documentation.

--- MTGAController/MTGAController.py
+++ MTGAController/MTGAController.py
@@ -1,9 +1,16 @@
 from ControllerInterface import Controller
+from MTGALogReader import MTGALogReader
 
 
 class MTGAController(Controller):
     def __init__(self):
-        print("Hello")
+        self.patterns = ['"type": "GREMessageType_GameStateMessage"', 'PregameSequence end']
+        self.log_reader = MTGALogReader(self.patterns)
+
+    def start_game(self) -> None:
+        self.log_reader.start_log_monitor()
+
+        # TODO: Add mouse movement to press the play button
 
 
 
--- MTGAController/MTGALogReader.py
+++ MTGAController/MTGALogReader.py
@@ -1,24 +1,54 @@
+import threading
+import time
+
+
 class MTGALogReader:
-    USER = "Elliot Roe"
-    LOG_PATH = "C:\\Users\\" + USER + "\\AppData\\LocalLow\\Wizards Of The Coast\\MTGA\\Player.log"
-    GAME_STATE_PATTERN = "GreToClientEvent"
 
-    # follow.py
-    #
-    # Follow a file like tail -f.
+    LOG_UPDATE_SPEED = 0.1
 
-    import time
-    def follow(thefile):
-        thefile.seek(0, 2)
-        while True:
-            line = thefile.readline()
+    def __init__(self, patterns, user = "Elliot Roe", player_id = "LE3ZCMCJZBHUDGATTY2EJLUEIM"):
+        self.__user = user
+        self.__player = player_id
+        self.__log_path = "/Users/" + self.__user + "/AppData/LocalLow/Wizards Of The Coast/MTGA/Player.log"
+        self.__lines_containing_pattern = {}
+        for pattern in patterns:
+            self.__lines_containing_pattern[pattern] = ""
+
+        self.__log_monitor_thread = None
+        self.__stop_monitor = False
+
+    def __follow(self, the_file):
+        the_file.seek(0, 2)
+        while not self.__stop_monitor:
+            line = the_file.readline()
             if not line:
-                time.sleep(0.1)
+                time.sleep(self.LOG_UPDATE_SPEED)
                 continue
             yield line
 
-    if __name__ == '__main__':
-        logfile = open("run/foo/access-log", "r")
-        loglines = follow(logfile)
-        for line in loglines:
-            print line,
+    def __monitor_log_file(self):
+        print(self.__log_path)
+        log_file = open(self.__log_path, "r")
+        log_lines = self.__follow(log_file)
+        for line in log_lines:
+            if self.__stop_monitor:
+                return
+            for pattern in self.__lines_containing_pattern:
+                if pattern in line:
+                    self.__lines_containing_pattern[pattern] = line
+
+    def start_log_monitor(self):
+        self.__stop_monitor = False
+        self.__log_monitor_thread = threading.Thread(target=self.__monitor_log_file)
+        self.__log_monitor_thread.start()
+
+    def stop_log_monitor(self):
+        self.__stop_monitor = True
+        self.__log_monitor_thread.join()
+
+    def is_monitoring(self):
+        return self.__log_monitor_thread is not None and self.__log_monitor_thread.is_alive()
+
+    def get_latest_line_containing_pattern(self, pattern):
+        return self.__lines_containing_pattern[pattern]
+
